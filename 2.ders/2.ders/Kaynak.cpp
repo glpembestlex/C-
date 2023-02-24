b9a17feb57c55468082174c2150c354843e8579a
glpembestlex <117893393+glpembestlex@users.noreply.github.com>
2023-02-24 13:22:35 +0300

    C++

---
+++ 2.ders/2.ders/Kaynak.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	string isim = "Ali KABA";
+	int yas = 21;
+	cout << "merhaba" << endl << endl << " Ogrencý Adý :" << isim << endl;
+	cout << "ogrencý yasý :" << yas; 
+	cout << endl; 
+	return 0; 
+}
