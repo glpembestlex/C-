2e07309877756e784c095a7aca2fe93a1d97f3c5
glpembestlex <117893393+glpembestlex@users.noreply.github.com>
2023-02-27 12:39:32 +0300

    C++

---
+++ 3.ders/Project1/Kaynak.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std;
+int main() {
+	string isim = "ali kaba";
+	int yas = 20;
+	char notortalamasý = 'A';
+	float sayýsalnotortalamasý = 3.20;
+	bool  sýnavagirdimi = true;
+
+	cout << "merhaba" << endl << endl << "öðrenci - adýsoyadý : " << isim << endl ; 
+	cout << "öðrenci yasý :" << yas  << endl;
+	cout << "not ortalamasý :" << notortalamasý << endl ;
+	cout << "öðrenci sayýsal not ortalmasý ;" << sayýsalnotortalamasý << endl; 
+	cout << "öðrenci sýnava girmimi : " << sýnavagirdimi << endl; 
+	
+	
+	cout << endl;
+	return 0;
+
+
+
+}
